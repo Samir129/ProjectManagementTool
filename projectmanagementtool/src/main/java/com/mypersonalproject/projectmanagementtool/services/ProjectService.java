68df0fb573f78f87326ef5d2d19ba5af7cc1e6fc
Samir <ms921849@student.nitw.ac.in>
2023-04-09 17:59:07 +0530

    Added PostMapping

---
+++ projectmanagementtool/src/main/java/com/mypersonalproject/projectmanagementtool/services/ProjectService.java
@@ -0,0 +1,19 @@
+package com.mypersonalproject.projectmanagementtool.services;
+
+import com.mypersonalproject.projectmanagementtool.domain.Project;
+import com.mypersonalproject.projectmanagementtool.repositories.ProjectRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProjectService {
+
+    @Autowired
+    private ProjectRepository projectRepository;
+
+    public Project saveOrUpdateProject (Project project){
+
+        //Logic
+        return projectRepository.save(project);
+    }
+}
