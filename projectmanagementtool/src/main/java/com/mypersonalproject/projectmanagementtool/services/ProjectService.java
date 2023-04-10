05e6bc5eb5aec69668bc40fbf24099438ad585c3
Samir <ms921849@student.nitw.ac.in>
2023-04-10 13:26:05 +0530

    Exception handling when project identifier already exists

--- projectmanagementtool/src/main/java/com/mypersonalproject/projectmanagementtool/services/ProjectService.java
+++ projectmanagementtool/src/main/java/com/mypersonalproject/projectmanagementtool/services/ProjectService.java
@@ -1,6 +1,7 @@
 package com.mypersonalproject.projectmanagementtool.services;
 
 import com.mypersonalproject.projectmanagementtool.domain.Project;
+import com.mypersonalproject.projectmanagementtool.exceptions.ProjectIdException;
 import com.mypersonalproject.projectmanagementtool.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,7 +14,12 @@ public class ProjectService {
 
     public Project saveOrUpdateProject (Project project){
 
-        //Logic
-        return projectRepository.save(project);
+        try {
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        }
+        catch(Exception e){
+            throw new ProjectIdException("Project ID '" + project.getProjectIdentifier().toUpperCase() + "' already exists.");
+        }
     }
 }
