39d3a1c23d97dffa18ebeaa23dfcdeaa7ef54d92
Samir <ms921849@student.nitw.ac.in>
2023-04-27 14:03:11 +0530

    Handle username already exists exception

--- projectmanagementtool/src/main/java/com/mypersonalproject/projectmanagementtool/services/UserService.java
+++ projectmanagementtool/src/main/java/com/mypersonalproject/projectmanagementtool/services/UserService.java
@@ -2,6 +2,7 @@ package com.mypersonalproject.projectmanagementtool.services;
 
 
 import com.mypersonalproject.projectmanagementtool.domain.User;
+import com.mypersonalproject.projectmanagementtool.exceptions.UsernameAlreadyExistsException;
 import com.mypersonalproject.projectmanagementtool.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -17,12 +18,19 @@ public class UserService {
     private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     public User saveUser (User newUser){
-        newUser.setPassword(bCryptPasswordEncoder.encode(newUser.getPassword()));
+        try{
+            newUser.setPassword(bCryptPasswordEncoder.encode(newUser.getPassword()));
 
-        // Username has to be unique (exception)
+            // Username has to be unique (exception)
+            newUser.setUsername(newUser.getUsername());
+            //Make Sure that password and confirmpassword match
+            // We don't persist or show the confirmpassword
+            return userRepository.save(newUser);
 
-        //Make Sure that password and confirmpassword match
-        // We don't persist or show the confirmpassword
-        return userRepository.save(newUser);
+        }
+        catch(Exception e){
+            throw new UsernameAlreadyExistsException("Username '" + newUser.getUsername() + "' already exists");
+        }
+
     }
 }
