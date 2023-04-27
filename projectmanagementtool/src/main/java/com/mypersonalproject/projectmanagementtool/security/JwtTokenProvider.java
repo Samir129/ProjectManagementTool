bd2a2f8c60f8231bad4741c95fe28dc3db42a5e3
Samir <ms921849@student.nitw.ac.in>
2023-04-28 00:54:41 +0530

    Generate token and login api request impl

---
+++ projectmanagementtool/src/main/java/com/mypersonalproject/projectmanagementtool/security/JwtTokenProvider.java
@@ -0,0 +1,36 @@
+package com.mypersonalproject.projectmanagementtool.security;
+
+import com.mypersonalproject.projectmanagementtool.domain.User;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class JwtTokenProvider {
+
+    public String generateToken(Authentication authentication){
+        User user = (User)authentication.getPrincipal();
+        Date now = new Date(System.currentTimeMillis());
+
+        Date expiryDate = new Date(now.getTime() + Constants.EXPIRATION_TIME);
+
+        String userId = Long.toString(user.getId());
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("id", (Long.toString(user.getId())));
+        claims.put("username", user.getUsername());
+        claims.put("fullName", user.getFullName());
+
+        return Jwts.builder()
+                .setSubject(userId)
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(expiryDate)
+                .signWith(SignatureAlgorithm.HS512, Constants.SECRET)
+                .compact();
+    }
+}
--- projectmanagementtool/src/main/java/com/mypersonalproject/projectmanagementtool/web/UserController.java
+++ projectmanagementtool/src/main/java/com/mypersonalproject/projectmanagementtool/web/UserController.java
@@ -1,12 +1,21 @@
 package com.mypersonalproject.projectmanagementtool.web;
 
 import com.mypersonalproject.projectmanagementtool.domain.User;
+import com.mypersonalproject.projectmanagementtool.payload.JWTLoginSuccessResponse;
+import com.mypersonalproject.projectmanagementtool.payload.LoginRequest;
+import com.mypersonalproject.projectmanagementtool.security.Constants;
+import com.mypersonalproject.projectmanagementtool.security.JwtAuthenticationEntryPoint;
+import com.mypersonalproject.projectmanagementtool.security.JwtTokenProvider;
 import com.mypersonalproject.projectmanagementtool.services.MapValidationErrorService;
 import com.mypersonalproject.projectmanagementtool.services.UserService;
 import com.mypersonalproject.projectmanagementtool.validator.UserValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -27,6 +36,31 @@ public class UserController {
 
     @Autowired
     private UserValidator userValidator;
+
+    @Autowired
+    private JwtTokenProvider jwtTokenProvider;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+
+    @PostMapping("/login")
+    public ResponseEntity<?> loginRequest(@Valid @RequestBody LoginRequest loginRequest, BindingResult result){
+        ResponseEntity<?> errorMap = mapValidationErrorService.mapErrorValidation(result);
+        if(errorMap != null) return errorMap;
+
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginRequest.getUsername(),
+                        loginRequest.getPassword()
+                )
+        );
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = Constants.TOKEN_PREFIX + jwtTokenProvider.generateToken(authentication);
+
+        return ResponseEntity.ok(new JWTLoginSuccessResponse(true, jwt));
+    }
 
     @PostMapping("/register")
     public ResponseEntity<?> registerUser(@Valid @RequestBody User user, BindingResult result){
